d64d5a5cba8345ca0718be6e0b6b51c5963eac28
Asakuri <phungtanphat23@gmail.com>
2023-05-04 14:26:34 +0700

    Remove email and phone.

--- app/src/main/java/com/example/deannhom/fragment/EditInfo/EditInfoFragment.java
+++ app/src/main/java/com/example/deannhom/fragment/EditInfo/EditInfoFragment.java
@@ -32,8 +32,6 @@ public class EditInfoFragment extends Fragment {
         firebaseAuth = FirebaseAuth.getInstance();
 
         binding.btnConfirmed.setOnClickListener(view -> {
-            AtomicBoolean isUpdatingEmail = new AtomicBoolean(false);
-
             FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
             if (firebaseUser == null) {
                 return;
@@ -41,41 +39,15 @@ public class EditInfoFragment extends Fragment {
 
             String firstName = Objects.requireNonNull(binding.inputUserFirstName.getText()).toString();
             String lastName = Objects.requireNonNull(binding.inputUserLastName.getText()).toString();
-            String userEmail = Objects.requireNonNull(binding.inputUserEmail.getText()).toString();
-            String userPhone = Objects.requireNonNull(binding.inputUserPhone.getText()).toString();
 
             UserProfileChangeRequest.Builder builder = new UserProfileChangeRequest.Builder();
 
-            // TODO: Finish this too, case missing first or missing last
             if (!firstName.isEmpty() && !lastName.isEmpty()) {
                 builder.setDisplayName(lastName + " " + firstName);
             }
 
-            if (!userEmail.isEmpty()) {
-                isUpdatingEmail.set(true);
-                firebaseUser.updateEmail(userEmail).addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        isUpdatingEmail.set(false);
-                    }
-
-                    if (task.getException() != null) {
-
-                        task.getException().getMessage();
-                    }
-                });
-            }
-
-            // TODO: Finish this
-//            if (!userPhone.isEmpty()) {
-//
-//                firebaseUser.updatePhoneNumber();
-//            }
-
             firebaseUser.updateProfile(builder.build()).addOnCompleteListener(task -> {
                 if (task.isSuccessful()) {
-                    while (isUpdatingEmail.get()) {
-                    }
-
                     Toast.makeText(this.requireContext(), "Update profile successfully!", Toast.LENGTH_SHORT).show();
                     NavHostFragment.findNavController(this).navigate(R.id.action_navigation_edit_info_to_navigation_account);
                 }
